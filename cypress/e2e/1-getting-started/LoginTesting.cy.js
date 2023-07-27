ca71560064760f4eb4effbed18b739d3b52d909f
Agnes <agnes.realm@gmail.com>
2023-07-27 14:48:36 +0100

    Negative scenario - wrong password

--- cypress/e2e/1-getting-started/LoginTesting.cy.js
+++ cypress/e2e/1-getting-started/LoginTesting.cy.js
@@ -32,7 +32,7 @@ describe(" Verify Login functionality", () => {
     cy.get(":nth-child(1) > .oxd-main-menu-item > .oxd-text");
   });
 
-  it("negative login scenario", () => {
+  it("negative login scenario - blank password", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
@@ -43,5 +43,23 @@ describe(" Verify Login functionality", () => {
     cy.get("input[placeholder='Password']").type(" ");
 
     cy.get(".oxd-input-group > .oxd-text").should("have.text", "Required");
+  });
+
+  it("negative login scenario - wrong password", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("input[placeholder='Username']").click();
+    cy.get("input[placeholder='Username']").type("Adminn");
+
+    cy.get("input[placeholder='Password']").click();
+    cy.get("input[placeholder='Password']").type("admin1233");
+
+    cy.get("button[type='submit']").click();
+
+    cy.get(".oxd-text.oxd-text--p.oxd-alert-content-text").should(
+      "have.text",
+      "Invalid credentials"
+    );
   });
 });
